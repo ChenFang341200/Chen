6b9067e64972e2d4f0c5de1d254840895467c36d
ChenFang341200 <49309713+ChenFang341200@users.noreply.github.com>
2019-04-05 22:31:51 +0800

    Update GitTest.java

--- untitled/src/GitTest.java
+++ untitled/src/GitTest.java
@@ -2,13 +2,11 @@ public class GitTest {
 
 
     public static void main(String[] args) {
-        System.out.println("hello git");
       
-        System.out.println("day1");
+ System.out.println("我");
+       
 
-        System.out.println("我想提交到主干");
-
-        System.out.println("减肥");
+      
 
     }
 }
