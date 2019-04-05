9e9bf3d5d0ac62153202c9062343ae54d8f4730a
Chen <1258588080@qq.com>
2019-04-05 22:26:34 +0800

    x

--- untitled/src/GitTest.java
+++ untitled/src/GitTest.java
@@ -3,7 +3,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
-      
+        System.out.println("hello git");
         System.out.println("day1");
 
         System.out.println("我想提交到主干");
